d3a1d162afac85a7f16c66feac719c0b3732b66b
Vlad Kochenkov <kochenkov1993@gmail.com>
2020-11-20 01:11:12 +0300

    рабочий прототип. Пушу для сохранности

--- app/src/main/java/com/vkochenkov/snakegame/GameScreenActivity.java
+++ app/src/main/java/com/vkochenkov/snakegame/GameScreenActivity.java
@@ -2,9 +2,7 @@ package com.vkochenkov.snakegame;
 
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameScreenActivity extends AppCompatActivity {
+public class GameScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     //views
     private GameScreenView gameScreenView;
@@ -34,62 +32,80 @@ public class GameScreenActivity extends AppCompatActivity {
     private int startY;
     private int boardSize = 1;
     private List<Rect> snake = new ArrayList<>();
-
-    //other
-    private DisplayMetrics metrics;
-    private int phoneScreenHeight;
-    private int phoneScreenWidth;
-
     private int gameScreenSize;
+
+    private int phoneScreenWidth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
 
-        //find display size
-        metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        phoneScreenHeight = metrics.heightPixels;
-        phoneScreenWidth = metrics.widthPixels;
+        btnRightMove = findViewById(R.id.btn_right);
+        btnLeftMove = findViewById(R.id.btn_left);
+        btnUpMove = findViewById(R.id.btn_up);
+        btnDownMove = findViewById(R.id.btn_down);
 
-        //init game screen params
-        rectSize = phoneScreenWidth/multiple;
-        gameScreenSize = boardSize + (rectSize+boardSize)*(multiple-1);
-        startX = rectSize + boardSize;
-        startY = rectSize + boardSize;
+        btnRightMove.setOnClickListener(this);
+        btnLeftMove.setOnClickListener(this);
+        btnUpMove.setOnClickListener(this);
+        btnDownMove.setOnClickListener(this);
 
-        //init snake array
-        for (int i=0; i<3; i++) {
-            snake.add(new Rect(startX-((rectSize +boardSize)*i), startY, (startX+ rectSize)-((rectSize +boardSize)*i), (startY+ rectSize)));
-        }
+        findDisplaySize();
+        initGameScreenParams();
+        initSnakeArray();
+        createAndShowGameScreenView();
+    }
 
-        //create gameScreenView
-        gameScreenView = new GameScreenView(this, snake);
+    private void createAndShowGameScreenView() {
+        gameScreenView = new GameScreenView(this, snake, rectSize, boardSize, multiple);
         LayoutParams params = new LayoutParams(gameScreenSize, gameScreenSize);
         params.gravity = Gravity.CENTER_HORIZONTAL;
         gameScreenView.setLayoutParams(params);
         linearLayout = findViewById(R.id.game_screen_layout);
         linearLayout.addView(gameScreenView);
+    }
 
-        //todo сделать 4 кнопки
-        //test button
-        btnRightMove = findViewById(R.id.btn_move);
-        btnRightMove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Rect first = snake.get(0);
-                snake.add(0, new Rect(first.left+ rectSize +boardSize, first.top, first.right+ rectSize +boardSize, first.bottom));
-                snake.remove(snake.size()-1);
-                gameScreenView.setSnake(snake);
-                gameScreenView.invalidate();
+    private void initSnakeArray() {
+        for (int i = 0; i < 3; i++) {
+            snake.add(new Rect(startX - ((rectSize + boardSize) * i), startY, (startX + rectSize) - ((rectSize + boardSize) * i), (startY + rectSize)));
+        }
+    }
 
-                Log.d("gameScreenHeight", String.valueOf(gameScreenView.getMeasuredHeight()));
-                Log.d("gameScreenWidth", String.valueOf(gameScreenView.getMeasuredWidth()));
-                //Log.d("snakeLength", String.valueOf(snake.size()));
-                //Log.d("lastElem", snake.get(0).left + " " + snake.get(0).top + " " + snake.get(0).right + " " + snake.get(0).bottom);
-            }
-        });
+    private void initGameScreenParams() {
+        rectSize = phoneScreenWidth / multiple;
+        gameScreenSize = boardSize + (rectSize + boardSize) * (multiple - 1);
+        startX = boardSize + (rectSize + boardSize) * (multiple / 2);
+        startY = boardSize + (rectSize + boardSize) * (multiple / 2);
+    }
+
+    private void findDisplaySize() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        //phoneScreenHeight = metrics.heightPixels;
+        phoneScreenWidth = metrics.widthPixels;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Rect first = snake.get(0);
+        switch (view.getId()) {
+            case (R.id.btn_right):
+                snake.add(0, new Rect(first.left + rectSize + boardSize, first.top, first.right + rectSize + boardSize, first.bottom));
+                break;
+            case (R.id.btn_left):
+                snake.add(0, new Rect(first.left - rectSize - boardSize, first.top, first.right - rectSize - boardSize, first.bottom));
+                break;
+            case (R.id.btn_up):
+                snake.add(0, new Rect(first.left, first.top  - rectSize - boardSize, first.right, first.bottom - rectSize - boardSize));
+                break;
+            case (R.id.btn_down):
+                snake.add(0, new Rect(first.left, first.top  + rectSize + boardSize, first.right, first.bottom + rectSize + boardSize));
+                break;
+        }
+        snake.remove(snake.size() - 1);
+        gameScreenView.setSnake(snake);
+        gameScreenView.invalidate();
     }
 }
 
--- app/src/main/java/com/vkochenkov/snakegame/GameScreenView.java
+++ app/src/main/java/com/vkochenkov/snakegame/GameScreenView.java
@@ -5,63 +5,71 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.util.AttributeSet;
 import android.view.View;
-
-import androidx.annotation.Nullable;
 
 import java.util.List;
 
 public class GameScreenView extends View {
 
     private List<Rect> snake;
+    private int multiple;
+    private int rectSize;
+    private int boardSize;
+    private int viewSize;
 
     public void setSnake(List<Rect> snake) {
         this.snake = snake;
     }
 
-    public GameScreenView(Context context, List<Rect> snake) {
+    public GameScreenView(Context context, List<Rect> snake, int rectSize, int boardSize, int multiple) {
         this(context);
         this.snake = snake;
+        this.boardSize = boardSize;
+        this.rectSize = rectSize;
+        this.multiple = multiple;
     }
 
     public GameScreenView(Context context) {
         super(context);
     }
 
-    public GameScreenView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public GameScreenView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        if (viewSize == 0) {
+            viewSize = this.getMeasuredWidth();
+        }
+
+        drawFrame(canvas);
+        drawTable(canvas);
         drawSnake(canvas);
     }
 
     private void drawSnake(Canvas canvas) {
-        drawFrame(canvas);
-
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
+        paint.setColor(Color.GREEN);
         paint.setStrokeWidth(1);
-        for (int i=0; i<snake.size(); i++) {
+        for (int i = 0; i < snake.size(); i++) {
             canvas.drawRect(snake.get(i), paint);
         }
     }
 
     private void drawFrame(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
+        paint.setColor(Color.GRAY);
         paint.setStrokeWidth(1);
-        Rect rect = new Rect(0, 0, this.getMeasuredWidth(), this.getMeasuredHeight());
+        Rect rect = new Rect(0, 0, viewSize, viewSize);
         canvas.drawRect(rect, paint);
     }
 
-    //todo drowTable(Canvas canvas) {}
-    //нариисовать таблицу в цикле, для удобства
+    private void drawTable(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(1);
+        for (int i = 0; i < multiple; i++) {
+            canvas.drawLine(0, (rectSize + boardSize) * i, viewSize, (rectSize + boardSize) * i, paint);
+            canvas.drawLine((rectSize + boardSize) * i, 0, (rectSize + boardSize) * i, viewSize, paint);
+        }
+    }
 }
