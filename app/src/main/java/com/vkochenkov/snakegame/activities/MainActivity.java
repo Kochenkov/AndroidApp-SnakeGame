4992d841f5cd911ded5f88326fa9461fe1d17b60
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-01-18 20:19:14 +0300

    добавил кнопку share, плюс уменьшил версию доступного сдк - перенес икнки из папки v24

--- app/src/main/java/com/vkochenkov/snakegame/activities/MainActivity.java
+++ app/src/main/java/com/vkochenkov/snakegame/activities/MainActivity.java
@@ -13,9 +13,11 @@ import com.vkochenkov.snakegame.R;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnPlay;
+    private Button btnExit;
+    private Button btnShare;
 //    private Button btnOptions;
 //    private Button btnInfo;
-    private Button btnExit;
+
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
         btnPlay = findViewById(R.id.btn_play);
         btnExit = findViewById(R.id.btn_exit);
+        btnShare = findViewById(R.id.btn_share);
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GameScreenActivity.class);
                 startActivity(intent);
+            }
+        });
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link_text));
+                startActivity(Intent.createChooser(intent, null));
             }
         });
         btnExit.setOnClickListener(new View.OnClickListener() {
